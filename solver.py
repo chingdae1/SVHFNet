fa302851b4bf8105cca42ed650d204d75809e17d
chingdae1 <chingdae0820@hanmail.net>
2019-04-13 13:58:21 +0900

    debug

--- solver.py
+++ solver.py
@@ -106,6 +106,8 @@ class Solver():
         with torch.no_grad():
             for step, (real_audio, face_a, face_b, labels) in enumerate(self.val_loader):
                 real_audio = real_audio.to(self.device)
+                print(real_audio)
+                print(torch.isnan(real_audio).sum().item())
                 face_a = face_a.to(self.device)
                 face_b = face_b.to(self.device)
                 labels = labels.to(self.device)
