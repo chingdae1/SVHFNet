f985213b6ef0e63d0d635f78fd60586bd0039eff
chingdae1 <chingdae0820@hanmail.net>
2019-05-20 16:51:51 +0900

    model4:ResNet for visual stream, PASE for audio stream. Both are pretrained

--- model/model4.py
+++ model/model4.py
@@ -1,81 +1,45 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from submodule.resblock import Block, OptimizedBlock
+import sys
+import os
+sys.path.append(os.path.abspath('../../pase'))
+from pase.models.frontend import wf_builder
+import model3
 
 
-# RestNet for visual stream
-class ResNet(nn.Module):
-    def __init__(self, ch=64, activation=F.relu):
-        super(ResNet, self).__init__()
-        self.activation = activation
-        self.block1 = OptimizedBlock(3, ch)
-        self.block2 = Block(ch, ch * 2, activation=activation, downsample=True)
-        self.block3 = Block(ch * 2, ch * 4, activation=activation, downsample=True)
-        self.block4 = Block(ch * 4, ch * 8, activation=activation, downsample=True)
-        self.block5 = Block(ch * 8, ch * 16, activation=activation, downsample=True)
-        self.block6 = Block(ch * 16, ch * 16, activation=activation, downsample=False)
-
-    def forward(self, x):
-        h = x
-        h = self.block1(h)
-        h = self.block2(h)
-        h = self.block3(h)
-        h = self.block4(h)
-        h = self.block5(h)
-        h = self.block6(h)
-        h = F.relu(h)
-        h = torch.sum(h, (2, 3))  # Global sum pooling.
-        return h
-
+# RestNet for visual stream, PASE for audio stream / All network is pretrained.
 
 class AudioStream(nn.Module):
-    def __init__(self, N=8):
+    def __init__(self, pase):
         super().__init__()
-        self.body = nn.Sequential(
-            nn.Conv2d(in_channels=1, out_channels=96, kernel_size=(7, 7), stride=2, padding=1),
-            nn.BatchNorm2d(96), nn.ReLU(),
-            nn.MaxPool2d(kernel_size=(2, 2)),
-
-            nn.Conv2d(in_channels=96, out_channels=256, kernel_size=(5, 5), stride=2, padding=(0, 1)),
-            nn.BatchNorm2d(256), nn.ReLU(),
-            nn.MaxPool2d(kernel_size=(2, 2)),
-
-            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=(3, 3), padding=1),
-            nn.BatchNorm2d(256), nn.ReLU(),
-
-            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=(3, 3), padding=1),
-            nn.BatchNorm2d(256), nn.ReLU(),
-
-            nn.Conv2d(in_channels=256, out_channels=256, kernel_size=(3, 3)),
-            nn.BatchNorm2d(256), nn.ReLU(),
-            nn.MaxPool2d(kernel_size=(3, 2)),
-        )
-        self.fc6 = nn.Linear(9*1*256, 4096)
-        self.relu6 = nn.ReLU()
-        self.apool6 = nn.AvgPool2d((1, N))
-        self.fc7 = nn.Linear(4096, 1024)
-        self.relu7 = nn.ReLU()
+        self.pase = pase  # (B, 100, 300) for 3s audio
+        self.fc1 = nn.Linear(100*300, 4096)
+        self.relu1 = nn.ReLU()
+        self.fc2 = nn.Linear(4096, 1024)
+        self.relu2 = nn.ReLU()
 
     def forward(self, x):
-        x = self.body(x)  # (B, 256, 9, 8)
-        x = x.view(x.shape[0], -1, x.shape[3])  # (B, 2304, 8)
-        x_out_list = []
-        for i in range(x.shape[2]):
-            x_out = self.relu6(self.fc6(x[:, :, i]))
-            x_out_list.append(x_out)
-        x = torch.stack(x_out_list, dim=2)  # (B, 4096, 8)
-        x = self.apool6(x)
-        x = x.view(-1, 4096)
-        x = self.relu7(self.fc7(x))
+        x = self.pase(x)
+        x = x.view(x.shape[0], -1)
+        x = self.relu1(self.fc1(x))
+        x = self.relu2(self.fc2(x))
         return x
 
 
 class SVHFNet(nn.Module):
-    def __init__(self):
+    def __init__(self, res_ckpt_path, pase_cfg_path, pase_ckpt_path):
         super().__init__()
-        self.vis_stream = ResNet()
-        self.aud_stream = AudioStream()
+        m3 = model3.SVHFNet()
+        map_location = None if torch.cuda.is_available() else 'cpu'
+        check_point = torch.load(res_ckpt_path, map_location=map_location)
+        state_dict = check_point['net']
+        m3.load_state_dict(state_dict)
+        self.vis_stream = m3.vis_stream
+        pase = wf_builder(pase_cfg_path)
+        pase.load_pretrained(pase_ckpt_path, load_last=True, verbose=True)
+        self.aud_stream = AudioStream(pase)
+
         self.fc8 = nn.Linear(3072, 1024)
         self.bn8 = nn.BatchNorm1d(1024)
         self.relu8 = nn.ReLU()
@@ -95,9 +59,12 @@ class SVHFNet(nn.Module):
         return x
 
 if __name__ == '__main__':
+    pase_cfg_path = '../../pase/cfg/PASE.cfg'
+    pase_ckpt_path = '../../pase/PASE.ckpt'
+    res_ckpt_path = '../saved/model3_bn/model_16.pt'
     face_a = torch.empty((2, 3, 224, 224))
     face_b = torch.empty((2, 3, 224, 224))
-    audio = torch.empty((2, 1, 512, 300))
-    net = SVHFNet()
+    audio = torch.empty((2, 1, 48000))
+    net = SVHFNet(res_ckpt_path, pase_cfg_path, pase_ckpt_path)
     output = net(face_a, face_b, audio)
     print(output.shape)
